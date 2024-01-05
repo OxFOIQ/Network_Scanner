9960a31b0ac7413d0ec137b5e0e39cb9e45288a0
Ox <76553571+MedAmyyne@users.noreply.github.com>
2024-01-06 00:41:18 +0100

    Update QuestorNet.py

--- QuestorNet.py
+++ QuestorNet.py
@@ -2,24 +2,29 @@ import scapy.all as scapy
 import argparse
 import pyfiglet
 
+
 def Get_Elements():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", dest="target", help="IP address Range / Network Address  With Mask /8 , /16 ,/24 , /32 \n Example 192.168.1.0/24")
+    parser.add_argument("-t", "--target", dest="target", help="IP address Range / Network Address  With Mask /8 , /16 ,/24 , /32 \n\n\n Example 192.168.1.0/24")
     options = parser.parse_args()
     if not options.target:
         parser.error("[!!] Please specify a target IP address range or Use --help for more information.")
     return options
 
+
 def Scan(ip):
     arp_Request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_Request_broadcast = broadcast / arp_Request
-    answered_List = scapy.srp(arp_Request_broadcast, timeout=1, verbose=0)[0]
+    arp_Request_broadcast = broadcast/arp_Request
+    answered_List = scapy.srp(arp_Request_broadcast, timeout=1, verbose=False)[0]
+
     devices_List = []
+ 
     for element in answered_List:
-        devices_Dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        devices_List.append(devices_Dict)
+        device = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        devices_List.append(device)
     return devices_List
+
 
 def Display_Result(result_List):
     print("IP \t\t\t Mac Address \n ------------------------------------------- ")
@@ -31,5 +36,5 @@ if __name__ == "__main__":
     banner = pyfiglet.figlet_format("QuestorNet")
     print(banner)
     options = Get_Elements()
-    scanResult = Scan(options)
-    Display_Result(scanResult)
+    Display_Result(Scan(options.target))
+
