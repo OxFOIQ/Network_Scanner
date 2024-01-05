10910d2770dcf04d553ed7e4595b96269d85badb
Ox <76553571+MedAmyyne@users.noreply.github.com>
2024-01-05 14:36:12 +0100

    Update QuestorNet.py

--- QuestorNet.py
+++ QuestorNet.py
@@ -1,37 +1,34 @@
-
 import scapy.all as scapy
 import argparse
 import pyfiglet
 
-def Get_Elements () :
+def Get_Elements():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target" , dest="target", help="IP address Range / Network Address  With Mask /8 , /16 ,/24 , /32 \n Example 192.168.1.0/24")
+    parser.add_argument("-t", "--target", dest="target", help="IP address Range / Network Address  With Mask /8 , /16 ,/24 , /32 \n Example 192.168.1.0/24")
     options = parser.parse_args()
-    print(options)
+    if not options.target:
+        parser.error("[-] Please specify a target IP address range. Use --help for more information.")
+    return options
 
-
-def Scan (ip) :
+def Scan(ip):
     arp_Request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_Request_broadcast= broadcast / arp_Request
-    answered_List=scapy.srp (arp_Request_broadcast , timeout=1 , verbose=1)[0]
-    devices_List =[]
-    for element in answered_List :
-        devices_Dict = {"ip": element[0].psrc , "mac": element[1].hwsrc}
+    arp_Request_broadcast = broadcast / arp_Request
+    answered_List = scapy.srp(arp_Request_broadcast, timeout=1, verbose=0)[0]
+    devices_List = []
+    for element in answered_List:
+        devices_Dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         devices_List.append(devices_Dict)
-    return (devices_List)
+    return devices_List
 
-
-def Display_Result (result_List) :
+def Display_Result(result_List):
     print("IP \t\t\t Mac Address \n ------------------------------------------- ")
-    for device in result_List :
+    for device in result_List:
         print(device["ip"] + "\t\t" + device["mac"])
-
 
 if __name__ == "__main__":
     banner = pyfiglet.figlet_format("QuestorNet")
     print(banner)
     options = Get_Elements()
     scanResult = Scan(options.target)
-    print(scanResult)
-
+    Display_Result(scanResult)
